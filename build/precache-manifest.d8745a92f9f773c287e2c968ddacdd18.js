1a2a8eb7eecc98c0a8f76385f4ba632daa83c125
cccccyy <1518856229@qq.com>
2020-11-10 17:45:09 +0800

    tb

---
+++ build/precache-manifest.d8745a92f9f773c287e2c968ddacdd18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "887817daebae12b81f86f4bbdeec3e00",
+    "url": "/react-taobao/build/index.html"
+  },
+  {
+    "revision": "567193e71f8e63142080",
+    "url": "/react-taobao/build/static/css/2.3786585f.chunk.css"
+  },
+  {
+    "revision": "d8afc0cbdd631fc3c532",
+    "url": "/react-taobao/build/static/css/main.a1d7fa52.chunk.css"
+  },
+  {
+    "revision": "567193e71f8e63142080",
+    "url": "/react-taobao/build/static/js/2.20f91d60.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/react-taobao/build/static/js/2.20f91d60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8afc0cbdd631fc3c532",
+    "url": "/react-taobao/build/static/js/main.8968e4dc.chunk.js"
+  },
+  {
+    "revision": "092a489f2239d1b83a66",
+    "url": "/react-taobao/build/static/js/runtime-main.b6b9f60a.js"
+  }
+]);
